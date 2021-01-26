d7ec360bdce9641b16158205b3876bd75cc8f0d2
Adrian Kotlinski <adrian.kotlinski@script-studio.pl>
2021-01-26 21:47:38 +0100

    Add get color of figure method

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,22 @@
 
- import {ChessFigure, ChessColour} from '../src/model/figure.js';
+ import {ChessFigure, ChessColor} from '../src/model/figure.js';
  import { Bishop } from '../src/model/figures/bishop.js';
  import { Pawn } from '../src/model/figures/pawn.js';
  import { King } from '../src/model/figures/king.js';
  import { Knight } from '../src/model/figures/knight.js';
  import { Queen } from '../src/model/figures/queen.js';
  import { Rook } from '../src/model/figures/rook.js';
-const bishop1 = new Bishop(ChessFigure.Bishop, ChessColour.Black, [0,0]);
-const pawn1 = new Pawn(ChessFigure.Pawn, ChessColour.Black, [0,0]);
-const king1 = new King(ChessFigure.King, ChessColour.Black, [0,0]);
-const knight1 = new Knight(ChessFigure.Knight, ChessColour.Black, [0,0]);
-const queen1 = new Queen(ChessFigure.Queen, ChessColour.Black, [0,0]);
-const rook1 = new Rook(ChessFigure.Rook, ChessColour.Black, [0,0]);
+const bishop1 = new Bishop(ChessFigure.Bishop, ChessColor.Black, [0,0]);
+const pawn1 = new Pawn(ChessFigure.Pawn, ChessColor.Black, [0,0]);
+const king1 = new King(ChessFigure.King, ChessColor.Black, [0,0]);
+const knight1 = new Knight(ChessFigure.Knight, ChessColor.Black, [0,0]);
+const queen1 = new Queen(ChessFigure.Queen, ChessColor.Black, [0,0]);
+const rook1 = new Rook(ChessFigure.Rook, ChessColor.Black, [0,0]);
 
- bishop1.log();
- pawn1.log();
- king1.log();
- knight1.log();
- queen1.log();
- rook1.log();
+//  bishop1.log();
+//  pawn1.log();
+//  king1.log();
+//  knight1.log();
+//  queen1.log();
+//  rook1.log();
+console.log(pawn1.getColor());
--- src/model/figure.ts
+++ src/model/figure.ts
@@ -7,13 +7,13 @@ export enum ChessFigure {
     Rook,
 }
 
-export enum ChessColour{
+export enum ChessColor{
     Black,
     White,
 }
 
 export abstract class Figure{
-    constructor(protected figure:ChessFigure, protected colour:ChessColour, protected position:[number, number]) {}
+    constructor(protected figure:ChessFigure, protected color:ChessColor, protected position:[number, number]) {}
     getFigurePosition(): [number, number]{
         return [this.position[0], this.position[1]];
     };
@@ -21,6 +21,8 @@ export abstract class Figure{
         this.position[0] = set[0];
         this.position[1] = set[1];
     };
-
+    getColor(): string{
+        return this.color == ChessColor.Black ? "black" : "white";
+    }
 }
 
