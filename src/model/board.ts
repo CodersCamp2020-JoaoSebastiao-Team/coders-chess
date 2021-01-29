160e206a8ebe5bbacf84855b882f1619aaa1e0a6
Magdalena <sochamagdalena8@gmail.com>
2021-01-29 20:28:24 +0000

    home page + chess board

--- src/model/board.ts
+++ src/model/board.ts
@@ -1,3 +1,43 @@
-export class Board{
-    
+const board = <HTMLElement>document.querySelector(".board");
+const boardLetters = <HTMLElement>document.querySelector(".letters");
+const boardNumbers = <HTMLElement>document.querySelector(".numbers");
+let letters = ["a", "b", "c", "d","e", "f", "g", "h"];
+let index = 0;
+let black = false;
+let num= 1;
+
+for(let i = 0; i < 8; i++) {
+  let letter = document.createElement("li");
+  letter.textContent = letters[i];
+  boardLetters.appendChild(letter);
+  let numbers = document.createElement("li");
+  numbers.textContent = String(num);
+  num++;
+  boardNumbers.appendChild(numbers);
+
 }
+
+for (let i =1; i<= 64; i++){
+    const square = document.createElement("div");
+    if(black) {
+        square.classList.add("square");
+        square.classList.add("black");
+        index++;
+        black = !black;
+    } else {
+        square.classList.add("square");
+        square.classList.add("white");
+        index++;
+        black=!black;
+    }
+    board.appendChild(square);
+
+    if(index === 8) {
+        black = !black;
+        index = 0;
+    }
+}
+
+// export class Board{
+    
+// }
