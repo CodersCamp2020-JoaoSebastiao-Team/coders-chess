72b89b1658482fb3b97d7a7c3014a82a6ec9e4df
João Kiakumbo <14078333+JK-Sebastiao@users.noreply.github.com>
2021-01-30 22:29:34 +0100

    Release 0.0.0 (#6)
    * setup environment
    
    * setup environment
    
    * setup environment
    
    * home page style
    
    * home page + chess board
    
    * correcting boards coordinates
    
    * setting server for deployment
    
    * setting parcel bundler
    
    * setting parcel bundler
    
    Co-authored-by: Joao Sebastiao <joao.sebastiao@atos.net>
    Co-authored-by: Magdalena <sochamagdalena8@gmail.com>
    Co-authored-by: Magdalena Socha <74918281+magdalena-socha@users.noreply.github.com>

---
+++ src/main.ts
@@ -0,0 +1,42 @@
+// const rangeSlider = <HTMLInputElement>document.getElementById("rs-range-line");
+// const rangeBullet = <HTMLInputElement>document.getElementById("rs-bullet");
+
+
+// if(rangeSlider){
+//    rangeSlider.addEventListener("input", showSliderValue, false); 
+// }
+
+
+// function showSliderValue() {
+//     if (rangeSlider && rangeBullet){
+//         rangeBullet.innerHTML = (<HTMLInputElement>document.getElementById("rs-range-line")).value;
+//         const bulletPosition:number = parseFloat(rangeSlider.value) / parseFloat(rangeSlider.max);
+//         rangeBullet.style.left = (bulletPosition * 487) + "px";
+//     }
+  
+// }
+
+const range = <HTMLInputElement>document.getElementById("range");
+const bubble = <HTMLOutputElement>document.getElementById("bubble");
+
+
+if(range){
+   range.addEventListener("input", () => {
+    setBubble(range, bubble)}, false); 
+}
+
+function setBubble(range:HTMLInputElement, bubble:HTMLOutputElement) {
+    const val:number = parseFloat(range.value);
+  
+    const min:number = parseFloat(range.min) || 0;
+    const max:number =  parseFloat(range.max) || 100;
+  
+    const offset = Number(((val - min) * 100) / (max - min));
+  
+    bubble.textContent = String(val);
+  
+    bubble.style.left = `calc(${offset}% - 10px)`;
+    bubble.style.color= 'black';
+}
+
+
--- src/model/board.ts
+++ src/model/board.ts
@@ -1,3 +1,43 @@
-export class Board{
-    
+const board = <HTMLElement>document.querySelector(".board");
+const boardLetters = <HTMLElement>document.querySelector(".letters");
+const boardNumbers = <HTMLElement>document.querySelector(".numbers");
+let letters:string[] = ["A", "B", "C", "D","E", "F", "G", "H"];
+let index:number = 0;
+let black:boolean = false;
+let num:number = 8;
+
+for(let i = 0; i < 8; i++) {
+  let letter = document.createElement("li");
+  letter.textContent = letters[i];
+  boardLetters.appendChild(letter);
+  let numbers = document.createElement("li");
+  numbers.textContent = String(num);
+  num--;
+  boardNumbers.appendChild(numbers);
+
 }
+
+for (let i =1; i<= 64; i++){
+    const square = document.createElement("div");
+    if(black) {
+        square.classList.add("square");
+        square.classList.add("black");
+        index++;
+        black = !black;
+    } else {
+        square.classList.add("square");
+        square.classList.add("white");
+        index++;
+        black=!black;
+    }
+    board.appendChild(square);
+
+    if(index === 8) {
+        black = !black;
+        index = 0;
+    }
+}
+
+// export class Board{
+    
+// }
