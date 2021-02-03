be8627fde72518725622f345a2d80e32dcc31368
Adrian Kotlinski <kotlinski95@gmail.com>
2021-02-03 12:10:18 +0100

    Add knight figure directions

--- src/model/figures/knight.ts
+++ src/model/figures/knight.ts
@@ -1,18 +1,62 @@
-import {Figure} from "../figure.js";
+import { Figure } from "../figure.js";
 
-export class Knight extends Figure{
-    log(){
+export class Knight extends Figure {
+    log() {
         console.log("Knight!");
     }
-    moveFigure(){
+    moveFigure() {
         return true;
     };
-    captureFigure(){
+    captureFigure() {
         return true;
     };
-    showDirections(boardMatrix:Array<[number, number]>){
-        //console.log("Pawn board matrix: ",boardMatrix);
-        let Directions:Array<[number, number]> = [];
+    showDirections(boardMatrix: Array<[number, number]>) {
+        console.log("Pawn board matrix: ", boardMatrix);
+        let Directions: Array<[number, number]> = [];
+
+        if (!findFigure(boardMatrix, this.getFigurePosition(), [2, 1])) {
+            Directions.push([2, 1]);
+        }
+        if (!findFigure(boardMatrix, this.getFigurePosition(), [1, 2])) {
+            Directions.push([1, 2]);
+        }
+        if (!findFigure(boardMatrix, this.getFigurePosition(), [-1, 2])) {
+            Directions.push([-1, 2]);
+        }
+        if (!findFigure(boardMatrix, this.getFigurePosition(), [-2, 1])) {
+            Directions.push([-2, 1]);
+        }
+        if (!findFigure(boardMatrix, this.getFigurePosition(), [-2, -1])) {
+            Directions.push([-2, -1]);
+        }
+        if (!findFigure(boardMatrix, this.getFigurePosition(), [-1, -2])) {
+            Directions.push([-1, -2]);
+        }
+        if (!findFigure(boardMatrix, this.getFigurePosition(), [1, -2])) {
+            Directions.push([1, -2]);
+        }
+        if (!findFigure(boardMatrix, this.getFigurePosition(), [2, -1])) {
+            Directions.push([2, -1]);
+        }
+
+
+        function findFigure(boardMatrix: Array<[number, number]>, position: [number, number], value: [number, number]): boolean {
+            let elementExist = false;
+            boardMatrix.forEach(element => {
+                if ((element[0] === (position[0] + value[0])) && (element[1] === (position[1] + value[1]))) {
+                    elementExist = true;
+                }
+                if (position[0] + value[0] > 7 || position[0] + value[0] < 0 || position[1] + value[1] > 7 || position[1] + value[1] < 0) {
+                    elementExist = true;
+                }
+            });
+            if (elementExist) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
         return Directions;
     }
 }
--- src/model/figures/pawn.ts
+++ src/model/figures/pawn.ts
@@ -13,24 +13,53 @@ export class Pawn extends Figure {
         return true;
     };
     showDirections(boardMatrix: Array<[number, number]>) {
-        //console.log("Pawn board matrix: ", boardMatrix);
+        console.log("Pawn board matrix: ", boardMatrix);
         let Directions: Array<[number, number]> = [];
         if (this.getColor() == "white") {
             if (this.getFigurePosition()[1] == 6) {
-                Directions.push([0, -2]);
-                Directions.push([0, -1]);
+                if (!findFigure(boardMatrix, this.getFigurePosition(), [0, -2])){
+                    Directions.push([0, -2]);
+                }
+                if (!findFigure(boardMatrix, this.getFigurePosition(), [0, -1])){
+                    Directions.push([0, -1]);
+                }
             }
             else if (this.getFigurePosition()[1] != 0) {
-                Directions.push([0, -1]);
+                if (!findFigure(boardMatrix, this.getFigurePosition(), [0, -1])){
+                    Directions.push([0, -1]);
+                }
             }
         }
         else {
             if (this.getFigurePosition()[1] == 1) {
-                Directions.push([0, 2]);
-                Directions.push([0, 1]);
+                if (!findFigure(boardMatrix, this.getFigurePosition(), [0, 2])){
+                    Directions.push([0, 2]);
+                }
+                if (!findFigure(boardMatrix, this.getFigurePosition(), [0, 1])){
+                    Directions.push([0, 1]);
+                }
             }
             else if (this.getFigurePosition()[1] != 7) {
-                Directions.push([0, 1]);
+                if (!findFigure(boardMatrix, this.getFigurePosition(), [0, 1])){
+                    Directions.push([0, 1]);
+                }
+            }
+        }
+        function findFigure(boardMatrix: Array<[number, number]>, position: [number,number], value: [number, number]): boolean{
+            let elementExist = false;
+            boardMatrix.forEach(element => {
+                if ((element[0] === (position[0]+value[0])) && (element[1] === (position[1]+value[1]))){
+                    elementExist = true;
+                }
+                if(position[0]+value[0]>7 || position[0]+value[0]<0 || position[1]+value[1]>7 || position[1]+value[1]<0){
+                    elementExist = true;
+                }
+            });
+            if (elementExist){
+                return true;
+            }
+            else{
+                return false;
             }
         }
         return Directions;
