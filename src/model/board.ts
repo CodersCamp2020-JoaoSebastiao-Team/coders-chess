ba708f4f9c1461aa6cfc9b9e13a71571827a3f30
tomsm <tomsmo007@gmail.com>
2021-02-11 16:50:33 +0100

    view of moves.

--- src/model/board.ts
+++ src/model/board.ts
@@ -64,9 +64,7 @@ let PlayerTour: ChessPlayerTour = ChessPlayerTour.White;
 console.log(`Player white begin.`);
 let previousNumber: number = -1;
 let previousFigure = gameFiguresArray[0];
-localStorage.removeItem('movesText');
-localStorage.removeItem('movesNotation');
-localStorage.removeItem('koniec');
+
 for (let i = 0; i < boardFields.length; i++) {
 
     boardFields[i].addEventListener("click", () => {
